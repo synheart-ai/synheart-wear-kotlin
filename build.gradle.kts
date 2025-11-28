70c8b4bf9eb9b17dd19d7dc4999e5c38fd9b9d00
yohannesTz <yohannes222ethiopia@gmail.com>
2025-11-28 22:03:00 +0300

    Revert "build: remove plugin version declarations for subproject compatibility"
    This reverts commit 932e120 to maintain JitPack compatibility.
    
    JitPack requires plugin versions to be declared for standalone builds.
    The SDK will be published to JitPack and consumed via Maven, not as
    a subproject dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.android.library") version "8.1.0"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id("maven-publish")
 }
 
